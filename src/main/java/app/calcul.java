4ce0408483f864c375bd3cd39f437bf2a4d31fb7
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-02-10 18:53:11 +0400

    #1

---
+++ src/main/java/app/calcul.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class calcul {
+    public static void main(String[] args) {
+        System.out.println("Input first number: ");
+        InputStream in = System.in;
+        Scanner scanner = new Scanner(in);
+        String num1 = scanner.nextLine();
+        int a = Integer.parseInt(num1);
+        System.out.println("Input second number: ");
+        String num2 = scanner.nextLine();
+        int b = Integer.parseInt(num2);
+        System.out.println("Input operation: ");
+        String op = scanner.nextLine();
+        System.out.println("Result: ");
+        switch(op){
+            case "+":
+                System.out.println(a+b);
+                break;
+            case "*":
+                System.out.println(a*b);
+                break;
+            case "/":
+                System.out.println(a/b);
+                break;
+            case "-":
+                System.out.println(a-b);
+                break;
+            default:
+                System.out.println("Wrong operation");
+        }
+
+
+    }
+}
