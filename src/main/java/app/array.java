91c1a531370676a0b6890f341d3e3964ad8b4315
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-02-12 16:34:06 +0400

    #1array

---
+++ src/main/java/app/array.java
@@ -0,0 +1,41 @@
+package app;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int[] even = new int [30];
+        int sum_even = 0;
+        int[] odd = new int [30];
+        int sum_odd = 0;
+        for(int i=0;i<30;){
+            int a = (int) (Math.random()*40-20);
+            if(a%2==0){
+                even[i]= a;
+                sum_even+=even[i];
+                i++;}
+        }
+        String evenString = Arrays.toString(even);
+        System.out.println(evenString);
+        System.out.println(sum_even);
+        for(int i=0;i<30;){
+            int a = (int) (Math.random()*60-30);
+            if(a%2!=0){
+                odd[i]= a;
+                sum_odd+=odd[i];
+                i++;}
+        }
+        String oddString = Arrays.toString(odd);
+        System.out.println(oddString);
+        System.out.println(sum_odd);
+
+
+
+
+
+    }
+
+
+
+
+}
