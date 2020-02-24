fb3c2b76b41e9c349feeab2b1e911116989ea3f8
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-02-24 18:02:43 +0400

    #3

--- src/main/java/app/Formatter/Formatter3.java
+++ src/main/java/app/Formatter/Formatter3.java
@@ -2,7 +2,7 @@ package app.Formatter;
 
 public class Formatter3 extends Formatter{
     void formatter(String s){
-        this.s=String.format("*******\n*Hello*\n*******");
+        this.s=String.format("*******\n*%s*\n*******",s.toUpperCase());
 
     }
 }
