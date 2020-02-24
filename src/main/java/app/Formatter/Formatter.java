3926e58498eb0d58f1bc2986f0415bea37028217
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-02-24 17:59:09 +0400

    #3

--- src/main/java/app/Formatter/Formatter.java
+++ src/main/java/app/Formatter/Formatter.java
@@ -3,4 +3,7 @@ package app.Formatter;
 public abstract class Formatter {
     String s;
     abstract void formatter(String s);
+    void print(String s){
+        System.out.println(this.s);
+    }
 }
--- src/main/java/app/Formatter/FormatterApp.java
+++ src/main/java/app/Formatter/FormatterApp.java
@@ -5,17 +5,20 @@ public class FormatterApp {
     f.s=s;
     f.formatter(s);
     System.out.println(f.s);
-
   }
-
   public static void main(String[] args) {
     Formatter fmt1 = new Formatter1();
     Formatter fmt2 = new Formatter2();
     Formatter fmt3 = new Formatter3();
+    System.out.println("First task");
     print("hello", fmt1); // hello
     print("hello", fmt2); // HELLO
     print("hello", fmt3); // ***********
                             // *  HELLO  *
                             // ***********
+    System.out.println("Second task");
+    fmt1.print("hello");    // hello
+    fmt2.print("hello");    // HELLO
+    fmt3.print("hello");
   }
 }
