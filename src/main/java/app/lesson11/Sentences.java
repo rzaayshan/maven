b18518369d52c001ef717da57bd2e30c34e4410c
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-03-02 18:26:44 +0400

    #3

---
+++ src/main/java/app/lesson11/Sentences.java
@@ -0,0 +1,26 @@
+package app.lesson11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sentences {
+    public static void main(String[] args) {
+        List<String> subjects = new ArrayList(Arrays.asList("Noel", "The cat", "The dog"));
+        List<String> verbs = new ArrayList(Arrays.asList("wrote", "chased", "slept on"));
+        List<String> objects =  new ArrayList(Arrays.asList("the book","the ball","the bed"));
+        List<String> sentences = new ArrayList<>();
+        for(String subject : subjects){
+            for(String verb: verbs){
+                for(String object: objects){
+                    String s = String.format("%s %s %s", subject, verb, object);
+                    sentences.add(s);
+                }
+            }
+        }
+        for(String el: sentences)
+            System.out.println(el);
+
+
+    }
+}
--- src/main/java/app/lesson12/Calculator.java
+++ src/main/java/app/lesson12/Calculator.java
@@ -21,27 +21,44 @@ public class Calculator {
         while (true){
             StringBuilder firstS = new StringBuilder();
             StringBuilder secondS = new StringBuilder();
-            int k=0,i;
-            boolean flag=false;
+            int i,k=0;
+            boolean flag1=false,flag2=false;
             String input = new Scanner(System.in).next();
             if(input.equals("quit"))
                 break;
-            if(String.valueOf(input.charAt(0)).equals("-")){
-                k++;
-                flag=true;}
-            for(i=k;i<input.length();i++){
-                if(!s.contains(String.valueOf(input.charAt(i))))
+            for(i=0;i<input.length();i++){
+                if(String.valueOf(input.charAt(i)).equals("(") ||
+                        String.valueOf(input.charAt(i)).equals(")")
+                )
+                    continue;
+                boolean contains = s.contains(String.valueOf(input.charAt(i)));
+                if(contains)
+                    firstS.append(input.charAt(i));
+                if(String.valueOf(input.charAt(i)).equals("-") && firstS.equals(null)){
+                    flag1=true;
+                    k++;
+                    continue;
+                }
+                if(!contains && !firstS.equals(null))
                     break;
-                firstS.append(input.charAt(i));
             }
-            char op=input.charAt(i);
-            for(i=i+1;i<input.length();i++){
-                secondS.append(input.charAt(i));
+            char op=input.charAt(i++);
+            for(int j=i;j<input.length();j++){
+                if(String.valueOf(input.charAt(j)).equals("(") ||
+                        String.valueOf(input.charAt(j)).equals(")")
+                )
+                    continue;
+                if(String.valueOf(input.charAt(j)).equals("-")){
+                    flag2=true;}
+                secondS.append(input.charAt(j));
             }
             int first=Integer.parseInt(firstS.toString());
-            if(flag)
-                first=-1*first;
             int second=Integer.parseInt(secondS.toString());
+            if(flag1)
+                first=-1*first;
+            if(flag2)
+                second=-1*second;
             System.out.println(operation(first,second,op));
+
     }}
 }
